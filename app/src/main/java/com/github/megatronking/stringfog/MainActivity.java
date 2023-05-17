be1f819dd361128081e4a164916d8ababc2c18db
MegatronKing <1256980529@qq.com>
2023-05-17 19:37:38 +0800

    remove test code

--- app/src/main/java/com/github/megatronking/stringfog/MainActivity.java
+++ app/src/main/java/com/github/megatronking/stringfog/MainActivity.java
@@ -1,7 +1,6 @@
 package com.github.megatronking.stringfog;
 
 import android.os.Bundle;
-import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,7 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.e("ok", "good");
         // 范例请参考：
         // https://github.com/MegatronKing/StringFog-Sample1
         // https://github.com/MegatronKing/StringFog-Sample2
